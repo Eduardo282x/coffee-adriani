26f7bdceb611c0e721c9fe67a085eeb33a00a656
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-08-25 01:30:54 -0400

    change dialog component close

--- src/components/dialog/DialogComponent.tsx
+++ src/components/dialog/DialogComponent.tsx
@@ -13,17 +13,32 @@ interface DialogProps {
 
 export const DialogComponent: FC<DialogProps> = ({ open, setOpen, className, isEdit, label1, label2, children }) => {
 
+    // const handleOpenChange = (isOpen: boolean) => {
+    //     if (!isOpen) {
+    //         // Si el dialog se cierra, mover el foco al body
+    //         (document.activeElement as HTMLElement)?.blur();
+    //     }
+    //     setOpen(isOpen);
+    // };
+
     const handleOpenChange = (isOpen: boolean) => {
-        if (!isOpen) {
-            // Si el dialog se cierra, mover el foco al body
-            (document.activeElement as HTMLElement)?.blur();
-        }
         setOpen(isOpen);
+        if (!isOpen) {
+            setTimeout(() => {
+                // Intenta mover el foco a un botón principal o al body
+                const mainBtn = document.querySelector('button, [tabindex]:not([tabindex="-1"])');
+                if (mainBtn) {
+                    (mainBtn as HTMLElement).focus();
+                } else {
+                    (document.body as HTMLElement).focus();
+                }
+            }, 0);
+        }
     };
 
     return (
         <Dialog open={open} onOpenChange={handleOpenChange} modal>
-            <DialogContent className={className}>
+            <DialogContent forceMount className={className}>
                 <DialogHeader>
                     <DialogTitle className='text-center'>
                         {isEdit ? label1 : label2}
