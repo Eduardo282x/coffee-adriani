a96e903a295f9f3f98d3e0bd39be7d8700087b04
Eduardo282x <eduardokunmixyeah@gmail.com>
2026-09-21 11:24:50 -0400

    fix: streamline date handling in DateRangePicker by removing unnecessary state and effects

--- src/components/datepicker/DateRangePicker.tsx
+++ src/components/datepicker/DateRangePicker.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 import { CalendarIcon } from 'lucide-react';
 // import { addDays, format } from 'date-fns';
 import { Label } from '../ui/label';
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 // import { es } from 'date-fns/locale';
 import { es } from "react-day-picker/locale";
 import { DateRange } from 'react-day-picker';
@@ -20,16 +20,7 @@ interface DateRangePickerProps {
 }
 
 export const DateRangePicker: FC<DateRangePickerProps> = ({ datePicker, setDatePicker, label, btnWidth, toDate }) => {
-    const [date, setDate] = useState<DateRange | undefined>(datePicker)
     const [open, setOpen] = useState<boolean>(false);
-
-    useEffect(() => {
-        setDatePicker(date as DateRange | undefined)
-    }, [date])
-
-    useEffect(() => {
-        setDate(datePicker)
-    }, [datePicker])
 
     return (
         <div>
@@ -44,33 +35,33 @@ export const DateRangePicker: FC<DateRangePickerProps> = ({ datePicker, setDateP
                         variant={"outline"}
                         className={cn(
                             "w-48 overflow-hidden justify-start text-left font-normal",
-                            !date && "text-muted-foreground",
+                            !datePicker && "text-muted-foreground",
                             btnWidth
                         )}
                     >
                         <CalendarIcon />
-                        {date?.from ? (
-                            date.to ? (
+                        {datePicker?.from ? (
+                            datePicker.to ? (
                                 <>
-                                    {format(date.from, "LLL dd, y", { locale: es })} -{" "}
-                                    {format(date.to, "LLL dd, y", { locale: es })}
+                                    {format(datePicker.from, "LLL dd, y", { locale: es })} -{" "}
+                                    {format(datePicker.to, "LLL dd, y", { locale: es })}
                                 </>
                             ) : (
-                                format(date.from, "LLL dd, y", { locale: es })
+                                format(datePicker.from, "LLL dd, y", { locale: es })
                             )
                         ) : (
                             <span>Selecciona una fecha</span>
                         )}
-                        {/* {date ? format(date, "PPP", { locale: es }) : <span>Selecciona una fecha</span>} */}
+                        {/* {datePicker ? format(datePicker, "PPP", { locale: es }) : <span>Selecciona una fecha</span>} */}
                     </Button>
                 </PopoverTrigger>
                 <PopoverContent className="w-auto p-0" align="start">
                     <Calendar
                         mode="range"
                         locale={es}
-                        selected={date}
-                        onSelect={setDate}
-                        defaultMonth={date?.from}
+                        selected={datePicker}
+                        onSelect={setDatePicker}
+                        defaultMonth={datePicker?.from}
                         captionLayout="dropdown"
                         toDate={toDate}
                     />
